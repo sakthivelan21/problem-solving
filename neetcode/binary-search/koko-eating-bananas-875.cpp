2f81add5e503ed0d7ab72c757bcf9c42ed21857b
Sakthi velan S <62512398+sakthivelan21@users.noreply.github.com>
2026-07-16 07:43:47 +0530

    [binary-search] koko banana

---
+++ neetcode/binary-search/koko-eating-bananas-875.cpp
@@ -0,0 +1,73 @@
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. 
+Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, 
+she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+*/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+
+        int max_value = INT_MIN;
+
+        for(int i=0;i<piles.size();i++)
+        {
+            max_value = max(max_value,piles[i]);
+        }
+
+        int right = max_value;
+
+        long long int hour =0; int mid=0;
+        int result=max_value;
+
+        while(left<=right)
+        {
+            mid = left + (right-left)/2;
+
+            hour=0;
+
+            for(int i=0;i<piles.size();i++)
+                hour+=ceil(static_cast<double>(piles[i])/mid);
+            
+            if(hour<=h)
+            {
+                result = min(result,mid);
+                right = mid-1;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+
+        return result;
+    }
+};
